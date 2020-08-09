89c3ef612144dadf4b8ded9c05ba9c90d9b3a949
Victor Cortés <cortes.victorhugo@gmail.com>
2020-08-09 18:23:59 -0500

    change cache name service worker

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("../service-worker.js")
     .then((swRegistered) => {
-      console.log("ServiceWorker registration successful!");
+      //console.log("ServiceWorker registration successful!");
       sw = swRegistered;
     })
     .catch((err) => console.log("ServiceWorker registration failed: ", err));
