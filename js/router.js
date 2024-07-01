0e4bdc9a4ef3828ae7104287ae3b51e3206b4e82
Victor Cortés <cortes.victorhugo@gmail.com>
2024-07-01 13:33:20 -0500

    chore: Remove google analytics and vwo

--- js/router.js
+++ js/router.js
@@ -30,69 +30,37 @@ const router = (route = '') => {
     case '#/':
     case '#/home':
     case '/index.html':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/home',
-        page_title: 'Inicio',
-      });
       removeActive();
       navHome.classList.add('main-nav__link--active');
       return content.appendChild(Home.home());
 
     case '#/about':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/about',
-        page_title: 'Sobre mí',
-      });
       removeActive();
       navAboutMe.classList.add('main-nav__link--active');
       return content.appendChild(AboutMe.aboutMe());
 
     case '#/tutorials':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/tutorials',
-        page_title: 'Tutoriales',
-      });
       removeActive();
       navTutorials.classList.add('main-nav__link--active');
       return content.appendChild(Tutorials.tutorials());
 
     case '#/articles':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/articles',
-        page_title: 'Artículos',
-      });
       removeActive();
       navArticles.classList.add('main-nav__link--active');
       return content.appendChild(Articles.articles());
 
     case '#/hobbies':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/hobbies',
-        page_title: 'Pasatiempos',
-      });
       removeActive();
       navHobbies.classList.add('main-nav__link--active');
       return content.appendChild(Hobbies.hobbies());
 
     case '#/remove-password':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/remove-password',
-        page_title: 'Remover contraseñas archivos de Excel',
-      });
       return content.appendChild(Home.removePass());
 
     case '#/modern-combat':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/modern-combat',
-        page_title: 'Tutorial MC5 - Abre los ojos 04 Rescate',
-      });
       return content.appendChild(Home.modernCombat());
 
     case '#/intro-hybrids':
-      gtag('config', 'G-C33X0C9F2V', {
-        page_path: '/intro-hybrids',
-        page_title: 'Introducción sobre Hybrids',
-      });
       return content.appendChild(Home.introHybrids());
 
     default:
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shell-v1.0.5";
+const CACHE_NAME = "shell-v1.0.6";
 const filesToCache = [
   "/",
   "./index.html",
